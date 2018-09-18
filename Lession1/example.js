12dfbc6a13a764437ab9e31b989619a22f0558e6
Hari Remo <remo.hari@isolutions.ch>
2018-09-18 16:56:58 +0200

    morse example added

---
+++ Lession1/example.js
@@ -0,0 +1,45 @@
+"use strict";
+console.log("adadsasdsf");
+var bla = 10;
+
+console.log(typeof bla);
+
+
+var square = function(x) {
+    return x*x
+}
+
+console.log(square(10));
+//Javascript ist function scoped (Nur Funktionen machen einen neuen scope)
+
+var alphabetString = 
+		"a=.-;b=-...;c=-.-.;d=-..;e=.;f=..-;g=--.;h=....;i=..;j=.---;k=-.-;l=.-..;"+
+		"m=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;"+
+        "y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..";
+var morseArray = {}; 
+alphabetString.split(";")
+    .forEach(function(x){
+        var arr = x.split('=');
+        arr[1] && (morseArray[arr[0]] = arr[1]);
+});
+
+function charToMorse(alphaChar) {
+    if(alphaChar in morseArray){
+        return morseArray[alphaChar];
+    }
+    return "";    
+}
+
+
+function convertToMorse(alphabetString) {
+    var result = "";
+    alphabetString.split("").forEach(element => {
+        result += charToMorse(element);
+    });
+    return result;
+}
+
+console.log(convertToMorse("aaaaa aaaaa"));
+
+
+
---
+++ Lession1/takeaway.md
@@ -0,0 +1,9 @@
+# Einführung Javascript
+
+Javascript ist function scoped (Nur Funktionen machen einen neuen Scope).
+Javascript hat einen Lexical-Scope -> Funktionen können auf Variablen der "Parent-Funktion" zugreifen. Nach aussen geht es, nach innen nicht
+
+
+Hoisting -> Variablen werden zu  begin initialisiert und nicht erst beim Aufruf
+
+Enclosure -> TODO
